ab6f78710723e794c14d0bfede6d41451b396fd9
mehedi02 <mehedi1670@gmail.com>
2021-02-10 21:04:04 +0600

    Initial Commit

---
+++ run_builder.py
@@ -0,0 +1,13 @@
+from collections import namedtuple, OrderedDict
+from itertools import product
+
+class RunBuilder():
+	@staticmethod
+	def get_runs(params):
+		Run = namedtuple("Run", params.keys())
+		runs = []
+
+		for v in product(*params.values()):
+			runs.append(Run(*v))
+
+		return runs
